db3cfc1957f91e0b44565293ed25cab6f3014aab
Miguel <condori.miguel99@hotmail.com>
2021-09-10 18:01:46 -0300

    agrego endpoints específicas para cada parámetro

--- src/main/java/com/example/demo/controllers/PersonajeController.java
+++ src/main/java/com/example/demo/controllers/PersonajeController.java
@@ -4,7 +4,6 @@ import com.example.demo.models.Personaje;
 import com.example.demo.services.PersonajeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,51 +12,63 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/characters")
+@CrossOrigin("*")
 public class PersonajeController {
 
     @Autowired
     private PersonajeService personajeService;
 
-    @GetMapping()
-    public ResponseEntity<List<Personaje>> getByFilter(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
-        System.out.println(name + age);
-        return ResponseEntity.status(HttpStatus.OK).body(personajeService.findByFiltro(name, age));
+    @GetMapping(params = "name")
+    public ResponseEntity<List<Personaje>> getByName(@RequestParam String name){
+        System.out.println(name);
+        return ResponseEntity.status(HttpStatus.OK).body(personajeService.findByNombre(name));
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<Personaje>> getAll(){
-//        List<Personaje> personajes = personajeService.findAll();
-//        if ( personajes.isEmpty())
-//            return ResponseEntity.notFound().build();
-//        return ResponseEntity.status(HttpStatus.OK).body(personajes);
-//    }
+    @GetMapping(params = "age")
+    public ResponseEntity<List<Personaje>> getByAge(@RequestParam int age){
+        System.out.println(age);
+        return ResponseEntity.status(HttpStatus.OK).body(personajeService.findByEdad(age));
+    }
+
+    @GetMapping(params = "movie")
+    public ResponseEntity<List<Personaje>> getByMovie(@RequestParam(name = "movie") Long idMovie){
+        System.out.println(idMovie);
+        return ResponseEntity.status(HttpStatus.OK).body(personajeService.findByPeliculaSerie(idMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Personaje>> getAll(){
+        List<Personaje> personajes = personajeService.findAll();
+        if ( personajes.isEmpty())
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.OK).body(personajes);
+    }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Optional<Personaje>> getOne(@PathVariable Long id){
         Optional<Personaje> personaje = personajeService.getPersonaje(id);
-        if (!personaje.isPresent()){
+        if (personaje.isEmpty()){
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.status(HttpStatus.OK).body(personaje);
     }
 
-
-
     @PostMapping
-    public ResponseEntity<Personaje> createPersonaje(@RequestBody Personaje personajeDTO){
+    public ResponseEntity<Personaje> create(@RequestBody Personaje personajeDTO){
         Personaje personaje = personajeService.createPersonaje(personajeDTO);
         return ResponseEntity.ok(personaje);
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<Personaje> update(@PathVariable Long id, @RequestBody Personaje p){
+        System.out.println(id);
         if (p != null)
             return ResponseEntity.status(HttpStatus.OK).body(personajeService.update(id, p));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @DeleteMapping(path = "/{id}")
-    public ResponseEntity<Personaje> deletePersonaje(@PathVariable Long id){
+    public ResponseEntity<Personaje> delete(@PathVariable Long id){
         Boolean personaje = personajeService.deletePersonaje(id);
         if (personaje) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
