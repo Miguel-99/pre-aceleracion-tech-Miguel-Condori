aeafd024153868a82d3f523eb25488fc3957ee8f
Miguel <condori.miguel99@hotmail.com>
2021-09-26 18:56:27 -0300

    refactorizo entidad con lombok y agrego propiedades a columnas

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,10 +1,18 @@
 package com.example.demo.models;
 
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 public class User {
@@ -13,57 +21,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 20)
     private String firstName;
+
+    @Column(length = 20)
     private String lastName;
+
+    @Column(unique = true, nullable = false, length = 50)
     private String email;
+
+    @Column(unique = true, nullable = false, length = 20)
     private String username;
+
+    @Column(nullable = false, length = 20)
     private String password;
-
-    public User(String firstName, String lastName, String email, String username, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
